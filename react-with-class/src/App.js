948df4cd5bf33bf3622a0a6048ebb3f647ff3415
dev-chloe <chloekim.4969@gmail.com>
2022-05-06 17:09:11 +0900

    Rendering the Movies

--- react-with-class/src/App.js
+++ react-with-class/src/App.js
@@ -1,21 +1,34 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movies";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   }
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year");
+    this.setState({ movies, isLoading: false });
+  }
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading: false})
-    }, 6000)
+    this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
       <div>
         {
-          isLoading ? "Loading..." : "We are ready"
+          isLoading ? "Loading..." : movies.map(movie => {
+            return <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summay={movie.summay}
+              poster={movie.poster}
+            />
+          })
         }
       </div>
     )
